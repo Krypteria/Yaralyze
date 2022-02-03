33e2239810d9201e127f9a1a085527be7df9f5e4
Krypteria <alejandroriveraln@gmail.com>
2022-02-03 11:29:25 +0100

    Mayor modularidad en la vista

---
+++ Yaralyze/app/src/main/java/com/example/yaralyze01/ui/analysis/outcomes/CompleteAnalysisOutcome.java
@@ -0,0 +1,25 @@
+package com.example.yaralyze01.ui.analysis.outcomes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yaralyze01.R;
+
+
+public class CompleteAnalysisOutcome extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_complete_analysis_outcome, container, false);
+    }
+}
